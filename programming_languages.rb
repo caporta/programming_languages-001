51954c9f736f8f15a43e5b2acc8d62f9b8d2b80c
Chris Aporta <christopher.aporta@gmail.com>
2015-09-20 01:56:38 -0400

    complete lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    value.each do |k, v|
+      if new_hash[k]
+        new_hash[k][:style] << key
+      else
+        v[:style] = [key]
+        new_hash[k] = v
+      end
+    end
+  end
+  new_hash
 end
